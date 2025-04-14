090bebc489c6a87db75ae35395ca01184912f918
Fleps031 <whaleluv3r@gmail.com>
2025-04-14 00:27:06 -0300

    title and logo

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -20,7 +20,7 @@ function Hero() {
             mb-8
             '
           >
-          <div className='flex h-[100%] text-white md:p-2 min-[0px]:px-4 min-[0px]:pt-10 min-[0px]:flex-col md:flex-row md:align-middle md: justify-center'>
+          <div className='flex h-[100%] text-white md:p-2 min-[0px]:px-4 min-[0px]:pt-10 min-[0px]:flex-col md:flex-row md:align-middle md: justify-center sm:justify-center'>
             <div className='flex flex-col min-[0px]:gap-10 sm:gap-8 md:w-[50%] md:h-[100%] min-[0px]:justify-center'>
               <div className='font-bold md:text-5xl min-[0px]:text-3xl text-center ml-10'>
                 <h1>Nós <u className='text-[#E60076]'>revolucionamos</u> a forma com que você ouve música.</h1>
@@ -29,7 +29,7 @@ function Hero() {
                 <h2>Com o aplicativo Luvelt Melodia, milhões de artistas e músicas estão apenas alguns toques de distância</h2> 
               </div>
             </div>
-            <div className='flex flex-row md:w-[50%] md:h-[100%]'>
+            <div className='flex flex-row md:w-[50%] md:h-[100%] sm:justify-center'>
               <img className="spinning-vinil w-[600px] h-[600px] min-[0px]:mt-0 sm:mt-30" src={Vinil}></img>
             </div>
           </div>
