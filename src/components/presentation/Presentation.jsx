c7f2235b943bad1a73271ff332260f2bb6057391
alecoldelf <alecolvet248@gmail.com>
2025-04-13 23:10:06 -0300

    responsividade arrumada

--- src/components/presentation/Presentation.jsx
+++ src/components/presentation/Presentation.jsx
@@ -18,7 +18,7 @@ function Presentation() {
             </div>
           </div>
 
-          <div className="flex flex-col m-4 gap-5 p-8 w-full sm:w-[500px] md:w-[600px] h-auto sm:h-[400px] md:h-[500px] rounded-lg card-review bg-white text-black transition-transform duration-300 ease-in-out hover:translate-y-[-4px] shadow-[0_10px_15px_#d772a6] hover:shadow-pink">
+          <div className="flex flex-col m-4 gap-5 p-8 w-full sm:w-[500px] md:w-[600px] h-auto sm:h-[500px] md:h-[500px] rounded-lg card-review bg-white text-black transition-transform duration-300 ease-in-out hover:translate-y-[-4px] shadow-[0_10px_15px_#d772a6] hover:shadow-pink">
             <h1 className="text-center text-[#E60076] text-2xl sm:text-3xl">Por que nos escolher?</h1>
             <p className="text-center text-sm sm:text-base">Com um som único e de qualidade, nossos usuários conseguem desfrutar da real experiência de se ouvir uma música com extrema qualidade. <br /> Para artistas completamente inovadores fornecemos um sistema intuitivo e fácil para publicação de músicas, facilitando o encontro de novos artistas e seus futuros ouvintes.</p>
             <div className="flex justify-center items-center bottom-5">
